a50a1289e5c6d20602bcc51c4d8d31ce44df63b0
zxc <codehackfox@gmail.com>
2021-09-02 12:17:50 +0800

    source bash

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # alias别名
-提供一键安装别名
+
+提供跨平台一键安装别名
+
 ```
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/xsoer/alias/master/install.sh)"
 ```
--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 curl https://raw.githubusercontent.com/xsoer/alias/master/.bashrc_aliases >> $HOME/.bashrc_aliases
 echo "[ -f $HOME/.bashrc_aliases ] && . $HOME/.bashrc_aliases" >> ~/.bashrc
+source $HOME/.bashrc
